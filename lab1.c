dddf5ba5daac2a2016440d41594b5cd0792f96fa
Darshan-K4444 <darshan.k@s.amity.edu>
2025-03-07 19:30:53 +0530

    calculater

--- lab1.c
+++ lab1.c
@@ -20,7 +20,7 @@ int main()
             break;
         case '/':
             if (num2 != 0)
-                printf("Result: %.2f\n", num1 / num2);
+                printf("Result: %.2f\n", num1/ num2);
             else
                 printf("Error! Division by zero.\n");
             break;
